cc71e0505537c6aa5295d9ee29c351ac66b85cdd
AndrewCogi <whgustlr0326@gmail.com>
2023-12-21 19:11:16 +0900

    [23.12.21] AccessLog findAll() List type으로 반환하도록 Override

--- src/main/java/com/enicom/board/kyoritsu/dao/repository/AccessLogRepository.java
+++ src/main/java/com/enicom/board/kyoritsu/dao/repository/AccessLogRepository.java
@@ -1,5 +1,7 @@
 package com.enicom.board.kyoritsu.dao.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.enicom.board.kyoritsu.dao.entity.AccessLog;
@@ -8,4 +10,7 @@ import com.enicom.board.kyoritsu.dao.entity.AccessLog;
  *  AccessLog entity를 사용하여 CRUD를 실시하는 Repository
 **/
 
-public interface AccessLogRepository extends CrudRepository<AccessLog, Long> {}
+public interface AccessLogRepository extends CrudRepository<AccessLog, Long> {
+    @Override
+    List<AccessLog> findAll();
+}
