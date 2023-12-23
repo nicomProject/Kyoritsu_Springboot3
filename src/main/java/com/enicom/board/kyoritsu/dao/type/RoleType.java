e5e6e08db2cebb5868e963e4082b698990f429a3
AndrewCogi <whgustlr0326@gmail.com>
2023-12-23 19:09:59 +0900

    [23.12.23] 일반 사용자 삭제

--- src/main/java/com/enicom/board/kyoritsu/dao/type/RoleType.java
+++ src/main/java/com/enicom/board/kyoritsu/dao/type/RoleType.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum RoleType {
-    USER("ROLE_USER", "일반 사용자", 2),
+    // USER("ROLE_USER", "일반 사용자", 2),
     ADMIN("ROLE_ADMIN", "관리자", 1),
     SYSTEM("ROLE_SYSTEM", "시스템 관리자", 0);
 
