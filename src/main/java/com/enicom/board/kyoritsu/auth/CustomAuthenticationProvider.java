faa201cd78f3d786a934f28146df5c2e1e1b0371
AndrewCogi <whgustlr0326@gmail.com>
2023-12-23 20:30:55 +0900

    [23.12.23] 사용자 존재 여부 및 사용자 상태 따라 인증 실패 처리 정상 동작하도록 수정

--- src/main/java/com/enicom/board/kyoritsu/auth/CustomAuthenticationProvider.java
+++ src/main/java/com/enicom/board/kyoritsu/auth/CustomAuthenticationProvider.java
@@ -8,7 +8,7 @@ import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -35,19 +35,28 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
         log.info("접속 아이디: {}", username);
 
-        // 사용자가 존재하지 않음
-        UserDetails member = memberDetailService.loadUserByUsername(username);
+        // 사용자가 존재하지 않음 (기본값 null 잡아줘야함!!)
+        MemberDetail member = null;
+        try {
+            member = memberDetailService.loadUserByUsername(username);
+        } catch(UsernameNotFoundException e) {
+            member = null;
+        }
         if(member == null) {
+            System.out.println("일치하는 사용자 아이디가 없습니다.");
             throw new BadCredentialsException("일치하는 사용자 아이디가 없습니다.");
         }
 
         // 사용자의 상태에 따라 인증 실패 처리 (비활성화 계정, 계정 잠김, 비밀번호 불일치)
-        if(!member.isEnabled()) {
+        if(!member.isEnabled()) { // 비밀번호 불일치 5회 초과 시 계정 잠김
+            System.out.println("사용이 중지된 계정입니다.\r\n관리자에게 문의하시기 바랍니다.");
             throw new DisabledException("사용이 중지된 계정입니다.\r\n관리자에게 문의하시기 바랍니다.");
         } else if (!member.isAccountNonLocked()) {
+            System.out.println("로그인 시도 가능 횟수를 초과했습니다.\r\n관리자에게 문의하시기 바랍니다.");
             throw new LockedException("로그인 시도 가능 횟수를 초과했습니다.\r\n관리자에게 문의하시기 바랍니다.");
         } else if (!this.passwordEncoder.matches(member.getPassword(), password)) {
-            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+            System.out.println("비밀번호가 일치하지 않습니다. 로그인 실패 5회 초과 시 계정이 비활성화됩니다. (현재 "+(member.getFailureCnt()+1)+" 회)");
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다. 로그인 실패 5회 초과 시 계정이 비활성화됩니다. (현재 "+(member.getFailureCnt()+1)+" 회)");
         }
 
         // 인증 성공 시, token 반환
