4df4b00fae79155117d122b79a41154c8c336c38
AndrewCogi <whgustlr0326@gmail.com>
2023-12-26 15:59:15 +0900

    [23.12.26] Introduction api 작성 및 package화

---
+++ src/main/java/com/enicom/board/kyoritsu/dao/repository/introductions/IntroductionsRepository.java
@@ -0,0 +1,21 @@
+package com.enicom.board.kyoritsu.dao.repository.introductions;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.enicom.board.kyoritsu.dao.entity.Content;
+
+/**
+ *  Content entity를 사용하여 CRUD를 실시하는 Repository
+**/
+
+public interface IntroductionsRepository extends CrudRepository<Content, Long> ,IntroductionsRepositoryCustom {
+    // Content entity 내의 deleteDate값이 null이 아닌 모든 Content 반환
+    List<Content> findAllByDeleteDateNull();
+    // Content entity 내의 recKey를 사용하여 Content entity 찾아 list 형태로 반환
+    List<Content> findAllByRecKey(Long recKey);
+    // Content entity 내의 recKey를 사용하여 Content entity 찾아 반환
+    Optional<Content> findByRecKey(Long recKey);
+}
