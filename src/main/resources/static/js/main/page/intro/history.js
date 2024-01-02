5beb73550d51fa1b3a31b46d7efcd1050bc31cb4
R&D <jjg@enicom.co.kr>
2024-01-02 16:12:27 +0900

    [240102] user include_page

--- src/main/resources/static/js/main/page/intro/history.js
+++ src/main/resources/static/js/main/page/intro/history.js
@@ -33,56 +33,10 @@ $(function () {
             this.event();
         },
         event: function () {
-
-            var list_a = '.list-a';
-            var list_a_pc = '.pc_view .list-a';
-            var list_a_sp = '.sp_view .list-a';
-            var list_b = '.list-b';
-            var list_b_pc = '.pc_view .list-b';
-            var list_b_sp = '.sp_view .list-b';
-            var list_c = '.list-c';
-            var list_c_pc = '.pc_view .list-c';
-            var list_c_sp = '.sp_view .list-c';
             var list_d = '.list-d';
             var list_d_pc = '.pc_view .list-d';
             var list_d_sp = '.sp_view .list-d';
             $(function () {
-                $(list_a_pc).next().hide();
-                $(list_a_sp).next().hide();
-                $(list_a).on('click', function () {
-                    $(list_a).next().slideToggle(200);
-                    $(list_a).toggleClass('close', 200);
-                });
-                $('.pc_view ' + list_a).on('click', function () {
-                    $("html,body").animate({scrollTop:$('.pc_view .history-list-title.list-a').offset().top - 150});
-                })
-                $('.sp_view ' + list_a).on('click', function () {
-                    $("html,body").animate({scrollTop:$('.sp_view .history-list-title.list-a').offset().top - 70});
-                })
-                $(list_b_pc).next().hide();
-                $(list_b_sp).next().hide();
-                $(list_b).on('click', function () {
-                    $(list_b).next().slideToggle(200);
-                    $(list_b).toggleClass('close', 200);
-                });
-                $('.pc_view ' + list_b).on('click', function () {
-                    $("html,body").animate({scrollTop:$('.pc_view .history-list-title.list-b').offset().top - 150});
-                })
-                $('.sp_view ' + list_b).on('click', function () {
-                    $("html,body").animate({scrollTop:$('.sp_view .history-list-title.list-b').offset().top - 70});
-                })
-                $(list_c_pc).next().hide();
-                $(list_c_sp).next().hide();
-                $(list_c).on('click', function () {
-                    $(list_c).next().slideToggle(200);
-                    $(list_c).toggleClass('close', 200);
-                });
-                $('.pc_view ' + list_c).on('click', function () {
-                    $("html,body").animate({scrollTop:$('.pc_view .history-list-title.list-c').offset().top - 150});
-                })
-                $('.sp_view ' + list_c).on('click', function () {
-                    $("html,body").animate({scrollTop:$('.sp_view .history-list-title.list-c').offset().top - 70});
-                })
                 $(list_d_pc).next().hide();
                 $(list_d_sp).next().hide();
                 $(list_d).on('click', function () {
@@ -95,16 +49,6 @@ $(function () {
                 $('.sp_view ' + list_d).on('click', function () {
                     $("html,body").animate({scrollTop:$('.sp_view .history-list-title.list-d').offset().top - 70});
                 })
-                /*
-                  $('.sp_view .history-list-container .history-item .center').on('click', function () {
-                    $(this).next().slideToggle(200);
-                    $(this).parent().toggleClass('open', 200);
-                  });
-                  $('.sp_view .history-list-container .history-item .close').on('click', function () {
-                    $(this).parent().slideToggle(200);
-                    $(this).parent().parent().toggleClass('open', 200);
-                  });
-                */
             });
 
             $(function () {
@@ -119,25 +63,6 @@ $(function () {
                     });
                 });
             });
-
-            // window.onload = function () {
-            //     scroll_effect();
-            //
-            //     $(window).scroll(function () {
-            //         scroll_effect();
-            //     });
-            //
-            //     function scroll_effect() {
-            //         $('.effect-fade').each(function () {
-            //             var elemPos = $(this).offset().top;
-            //             var scroll = $(window).scrollTop();
-            //             var windowHeight = $(window).height();
-            //             if (scroll > elemPos - windowHeight) {
-            //                 $(this).addClass('effect-scroll');
-            //             }
-            //         });
-            //     }
-            // };
 
             $(function () {
                 scroll_effect();
