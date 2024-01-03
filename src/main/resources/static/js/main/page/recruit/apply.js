6b2eabdf9d872031dc9a80d9f75a27884da8f5b0
AndrewCogi <whgustlr0326@gmail.com>
2024-01-03 14:50:37 +0900

    [24.01.03] 자기소개서 글자 개수 카운팅 및 제한 기능 수정 & 필수입력 확인 기능 추가

--- src/main/resources/static/js/main/page/recruit/apply.js
+++ src/main/resources/static/js/main/page/recruit/apply.js
@@ -7,53 +7,100 @@ $(function () {
             const card = $('.submit-btn');
             card.find('*[role="action"]').click(function(e){
                 const action = this.dataset.action;
-                if(action === 'submit'){
-                    var birthDate = $("#birth").val();
-                    // var birthDate = "2023-12-12";
+                const answer = $("#answer")
+                
+                if (!answer[0].checked) {
+                    alert("지원 유의 사항을 확인 해 주세요.")
+                    answer.focus()
+                    return
+                }
+                if(action === 'submit' && answer){
+                    var birth = $("#birth")
+                    var birthDate = birth.val();
 
-                    var jobId = $("#job").val();
-                    // var jobId = 3;
+                    var job = $("#job");
+                    var jobId = job.val();
 
-                    var category = $("#category").val();
-                    // var category = "category";
+                    var category = $("#category");
+                    var categoryData = category.val();
 
-                    var email = $("#email").val();
-                    // var email = "email";
+                    var email = $("#email");
+                    var emailData = email.val();
 
                     var gender = $("#man")[0].checked == true ? "male" : "female";
-                    // var gender = "male"
 
-                    var name = $("#name").val();
-                    // var name = "name";
+                    var name = $("#name");
+                    var nameData = name.val();
                     
-                    var nationality = $("#nation").val()
-                    // var nationality = "nation"
+                    var nation = $("#nation")
+                    var nationality = nation.val()
 
-                    var mobile = $("#mobile").val();
-                    // var mobile = "mobile";
+                    var mobile = $("#mobile");
+                    var mobileData = mobile.val();
 
-                    var career = $("#career").val();
-                    // var career = "newcomer"
+                    var career = $("#career");
+                    var careerData = career.val();
 
                     var address = $("#address").val();
                     var addressDetail = $("#addressDetail").val();
 
-                    var contents = $("#contents").val();
-                    // var contents = "contents";
+                    var contents = $("#contents");
+                    var contentsData = contents.val();
+
+                    if (birthDate.length == 0) {
+                        alert("생년월일은 필수 데이터입니다.")
+                        birth.focus()
+                        return
+                    }
+                    else if (jobId.length == 0) {
+                        alert("채용공고 선택은 필수 데이터입니다.")
+                        job.focus()
+                        return
+                    }
+                    else if (categoryData.length == 0) {
+                        alert("카테고리 선택은 필수 데이터입니다.")
+                        category.focus()
+                        return
+                    }
+                    else if (emailData.length == 0) {
+                        alert("이메일은 필수 데이터입니다.")
+                        email.focus()
+                        return
+                    }
+                    else if (gender.length == 0) {
+                        alert("성별은 필수 데이터입니다.")
+                        gender.focus()
+                        return
+                    }
+                    else if (nameData.length == 0) {
+                        alert("성함은 필수 데이터입니다.")
+                        name.focus()
+                        return
+                    }
+                    else if (mobileData.length == 0) {
+                        alert("휴대폰 번호는 필수 데이터입니다.")
+                        mobile.focus()
+                        return
+                    }
+                    else if (contentsData.length == 0) {
+                        alert("자기소개서는 필수 데이터입니다.")
+                        contents.focus()
+                        return
+                    }
 
                     AjaxUtil.requestBody({
                         url: '/api/applicant/apply',
                         data: {
                             birthDate : birthDate,
                             jobId : jobId,
-                            category : category,
-                            email : email,
+                            category : categoryData,
+                            email : emailData,
                             gender : gender,
-                            name : name,
+                            name : nameData,
                             nationality : nationality,
-                            phone : mobile,
-                            career : career,
-                            contents : contents
+                            phone : mobileData,
+                            career : careerData,
+                            contents : contentsData
                         },
                         success: function (data) {
                             console.log(data)
@@ -64,7 +111,22 @@ $(function () {
                         }
                     })
                 }
-            })
+            });
+
+            const contentsField = $("#contents");
+            const contentsCount = $("#contentsCount");
+            const contentsMaxCount = $("#contentsMaxCount");
+            const countsMaxCountValue = contentsMaxCount[0].innerText;
+
+            contentsField.on('input propertychange', function() {
+                var cnt = (this.value.length);
+                contentsCount[0].innerText = cnt;
+
+                if (countsMaxCountValue < cnt) {
+                    alert("최대 입력 글자수를 초과하였습니다.\n글자수 제한: "+countsMaxCountValue)
+                    contentsField.val(contentsField.val().slice(0, countsMaxCountValue))
+                }
+            });
         }
     }
 
