0da20b527949e8f297225ed44d44b32953252eb5
R&D <jjg@enicom.co.kr>
2024-01-11 19:24:42 +0900

    [240111] footer 생성, index button 위치 수정

---
+++ src/main/resources/static/js/main/components/footer.js
@@ -0,0 +1,68 @@
+$(function () {
+    const Content = {
+        menus: [],
+        subMenus: [],
+        load: function () {
+            const that = this;
+            let menu_items = [];
+            let submenu_items = [];
+            this.menus = Menu.menus
+            this.subMenus = Menu.subMenus;
+
+            Object.keys(this.menus).forEach(key => {
+                const menuItems = {recKey: this.menus[key].recKey, name: this.menus[key].name, url: this.menus[key].url}
+                menu_items.push(menuItems);
+            });
+
+            Object.keys(this.subMenus).forEach(key => {
+                const submenuItems = {recKey: this.subMenus[key].menu.recKey, name: this.subMenus[key].name, url: this.subMenus[key].url}
+                submenu_items.push(submenuItems);
+            });
+
+            const container = $('.single-footer.f-contact.f-link');
+            container.html('');
+
+            const menuGroup = that.createMenuGroup(menu_items, submenu_items);
+            container.append(menuGroup);
+
+            this.draw()
+
+        },
+        draw: function () {
+
+            this.event();
+        },
+        event: function () {
+        },
+        createMenuGroup: function (menuItems, submenuItems) {
+            const menuGroup = $('.single-footer.f-contact.f-link');
+
+            menuItems.forEach(menuItem => {
+                const menuList = $('<ul class="footer_menu"></ul>');
+                const menuLink = $(`
+                <li>
+                <a>${menuItem.name}</a>
+                </li>
+        `);
+
+                menuList.append(menuLink);
+                submenuItems.forEach(submenuItem => {
+                    if (submenuItem.recKey === menuItem.recKey) {
+                        const submenuLi = $(`
+                    <li>
+                    <a href=${submenuItem.url}>${submenuItem.name}</a>
+                    </li>
+                `);
+                        menuList.append(submenuLi);
+                    }
+                });
+
+                menuGroup.append(menuList);
+            });
+
+            return menuGroup;
+        },
+    }
+
+    Content.load();
+});
