7a24dd48e87dacfbff6a6cb60b376742edde259e
R&D <jjg@enicom.co.kr>
2024-01-13 17:13:16 +0900

    [240113] 연혁 css수정, swiper js,

--- src/main/resources/static/js/main/page/intro/history.js
+++ src/main/resources/static/js/main/page/intro/history.js
@@ -33,55 +33,6 @@ $(function () {
             this.event();
         },
         event: function () {
-            var list_d = '.list-d';
-            var list_d_pc = '.pc_view .list-d';
-            var list_d_sp = '.sp_view .list-d';
-            $(function () {
-                $(list_d_pc).next().hide();
-                $(list_d_sp).next().hide();
-                $(list_d).on('click', function () {
-                    $(list_d).next().slideToggle(200);
-                    $(list_d).toggleClass('close', 200);
-                });
-                $('.pc_view ' + list_d).on('click', function () {
-                    $("html,body").animate({scrollTop:$('.pc_view .history-list-title.list-d').offset().top - 150});
-                })
-                $('.sp_view ' + list_d).on('click', function () {
-                    $("html,body").animate({scrollTop:$('.sp_view .history-list-title.list-d').offset().top - 70});
-                })
-            });
-
-            $(function () {
-                $(window).scroll(function () {
-                    $('.effect-fade').each(function () {
-                        var elemPos = $(this).offset().top;
-                        var scroll = $(window).scrollTop();
-                        var windowHeight = $(window).height();
-                        if (scroll > elemPos - windowHeight) {
-                            $(this).addClass('effect-scroll');
-                        }
-                    });
-                });
-            });
-
-            $(function () {
-                scroll_effect();
-
-                $(window).scroll(function () {
-                    scroll_effect();
-                });
-
-                function scroll_effect() {
-                    $('.effect-fade').each(function () {
-                        var elemPos = $(this).offset().top;
-                        var scroll = $(window).scrollTop();
-                        var windowHeight = $(window).height();
-                        if (scroll > elemPos - windowHeight) {
-                            $(this).addClass('effect-scroll');
-                        }
-                    });
-                }
-            });
 
             document.getElementById("btnOverview").addEventListener("click", function() {
                 window.location.href = '/intro/overview';
