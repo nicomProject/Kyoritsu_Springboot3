6a1bf0d30ab94ccdb26ae6c2c5e93cdbc49506f5
DalMuDee <whgustlr0326@gmail.com>
2024-01-15 02:56:15 +0900

    [24.01.14] Swiper 사용 오류 수정 (쓰이지 않아도 import하려는 문제 발생하여 수정)

--- src/main/resources/static/js/main/template/main.js
+++ src/main/resources/static/js/main/template/main.js
@@ -2,21 +2,24 @@
 
 
 	function initializeSwiper() {
-		var swiper = new Swiper(".swiper-container", {
-			slidesPerView: 1, // Set to 1 to show one slide at a time
-			centeredSlides: true,
-			autoplay: {
-				delay: 2500,
-				disableOnInteraction: false,
-			},
-			loop: false,
-			loopAdditionalSlides: 1,
-			pagination: false,
-			navigation: {
-				nextEl: ".swiper-button-next",
-				prevEl: ".swiper-button-prev",
-			},
-		});
+		// Swiper가 모든 main.js가 실행되는 페이지에 사용될 필요가 없음. 연쇄 오류를 방지하기 위해 해당 라이브러리가 포함된 곳에서만 동작하도록 수정
+		if(typeof Swiper !== "undefined") {
+			var swiper = new Swiper(".swiper-container", {
+				slidesPerView: 1, // Set to 1 to show one slide at a time
+				centeredSlides: true,
+				autoplay: {
+					delay: 2500,
+					disableOnInteraction: false,
+				},
+				loop: false,
+				loopAdditionalSlides: 1,
+				pagination: false,
+				navigation: {
+					nextEl: ".swiper-button-next",
+					prevEl: ".swiper-button-prev",
+				},
+			});
+		}
 	}
 
 	document.addEventListener('DOMContentLoaded', function () {
