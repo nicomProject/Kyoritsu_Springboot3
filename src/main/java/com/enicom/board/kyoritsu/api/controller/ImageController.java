501b1d86bc8dcbf048ce66a2fb27ff3c43170ab1
DalMuDee <whgustlr0326@gmail.com>
2024-01-18 23:47:32 +0900

    [24.01.18] smart editor 로컬 사진 업로드 구현

--- src/main/java/com/enicom/board/kyoritsu/api/controller/ImageController.java
+++ src/main/java/com/enicom/board/kyoritsu/api/controller/ImageController.java
@@ -12,8 +12,12 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.UUID;
 
 /**
  *  이미지 업로드 요청을 받고 처리함.
@@ -24,24 +28,6 @@ import java.text.SimpleDateFormat;
 @RequestMapping(path = "/api")
 @Slf4j
 public class ImageController {
-
-    // private final ImageService imageService;
-
-    // @RequestMapping(path = "/image", method = {RequestMethod.POST})
-    // // @ApiMapping(order = 24, desc = "[자료실] 이미지
-    // //
-    // // 업로드", param = RoomInfoParam.class)
-    // public ResponseHandler<?> uploadRoomImage(Model model, MultipartHttpServletRequest request, @RequestPart String name, @RequestPart MultipartFile file) {
-    //     model.addAttribute("path", request);
-    //     return new ResponseHandler<>(imageService.upload(request, name, file));
-    // }
-
-    // @RequestMapping(path = "/image", method = {RequestMethod.GET})
-    // // @ApiMapping(order = 25, desc = "[자료실] 이미지 다운로드", param = RoomInfoParam.class)
-    // public void downloadRoomImage(HttpServletRequest request, HttpServletResponse response, @RequestParam(name = "name") String name) {
-    //     imageService.download(request, response, name);
-    // }
-
 
     @RequestMapping(path = "/uploadImages", method = RequestMethod.POST)
     @ApiMapping(order = 23, desc = "[관리자] 각종 게시글 이미지 업로드")
@@ -96,4 +82,78 @@ public class ImageController {
         }
         return "It shouldn't be appeared!!!";
     }
+
+    @RequestMapping(value="/smarteditorMultiImageUpload")
+	public void smarteditorMultiImageUpload(HttpServletRequest request, HttpServletResponse response){
+		try {
+			//파일정보
+			String sFileInfo = "";
+			//파일명을 받는다 - 일반 원본파일명
+			String sFilename = request.getHeader("file-name");
+			//파일 확장자
+			String sFilenameExt = sFilename.substring(sFilename.lastIndexOf(".")+1);
+			//확장자를소문자로 변경
+			sFilenameExt = sFilenameExt.toLowerCase();
+				
+			//이미지 검증 배열변수
+			String[] allowFileArr = {"jpg","png","bmp","gif"};
+
+			//확장자 체크
+			int nCnt = 0;
+			for(int i=0; i<allowFileArr.length; i++) {
+				if(sFilenameExt.equals(allowFileArr[i])){
+					nCnt++;
+				}
+			}
+
+			//이미지가 아니라면
+			if(nCnt == 0) {
+				PrintWriter print = response.getWriter();
+				print.print("NOTALLOW_"+sFilename);
+				print.flush();
+				print.close();
+			} else {
+				//디렉토리 설정 및 업로드	
+				
+				//파일경로
+				String filePath = "./storage/images/";
+				File file = new File(filePath);
+				
+				if(!file.exists()) {
+					file.mkdirs();
+				}
+				
+				String sRealFileNm = "";
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+				String today= formatter.format(new java.util.Date());
+				sRealFileNm = today+UUID.randomUUID().toString() + sFilename.substring(sFilename.lastIndexOf("."));
+				String rlFileNm = filePath + sRealFileNm;
+				
+				// 서버에 파일쓰기
+				InputStream inputStream = request.getInputStream();
+				OutputStream outputStream=new FileOutputStream(rlFileNm);
+				int numRead;
+				byte bytes[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
+				while((numRead = inputStream.read(bytes,0,bytes.length)) != -1){
+					outputStream.write(bytes,0,numRead);
+				}
+				if(inputStream != null) {
+					inputStream.close();
+				}
+				outputStream.flush();
+				outputStream.close();
+				
+				// 정보 작성
+				sFileInfo += "&bNewLine=true";
+				sFileInfo += "&sFileName="+ sFilename;
+				sFileInfo += "&sFileURL=/"+filePath+sRealFileNm;
+				PrintWriter printWriter = response.getWriter();
+				printWriter.print(sFileInfo);
+				printWriter.flush();
+				printWriter.close();
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
